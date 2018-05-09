127d38063adaf439dbc7208e75469fd39393fffa
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2018-05-09 20:37:26 +0200

    We can use the gamecontroller for both players

--- Last_Resort/ModuleGameControllers.cpp
+++ Last_Resort/ModuleGameControllers.cpp
@@ -1,6 +1,7 @@
 #include "ModuleGameControllers.h"
 #include "Globals.h"
 #include "Application.h"
+#include "ModuleMainMenu.h"
 #include "ModuleInput.h"
 #include "SDL/include/SDL.h"
 
@@ -21,26 +22,10 @@ bool ModuleGameControllers::Init()
 
 	SDL_Init(SDL_INIT_GAMECONTROLLER);
 	
-	if (SDL_InitSubSystem(SDL_INIT_GAMECONTROLLER) < 0 || SDL_NumJoysticks() < 1) {
+	
+	if (SDL_InitSubSystem(SDL_INIT_GAMECONTROLLER) < 0) {
 		LOG("Error, gamecontroller fail to initilize",SDL_GetError());
 		
-	}
-
-
-	for (int i = 0; i < SDL_NumJoysticks(); i++){
-
-		if (i > MAX_GAME_CONTROLLERS) {
-			break;
-		}
-
-		if (SDL_IsGameController(i)){
-
-			PlayerController[i] = SDL_GameControllerOpen(i);//initializing the first controller
-			LOG("%d",SDL_GameControllerName);
-			LOG(SDL_GameControllerMapping(PlayerController[i]));
-			
-		}
-
 	}
 
 	//SDL_GameControllerAddMappingsFromFile(""); -> for adding or own map controller just leaving it here
@@ -53,56 +38,153 @@ bool ModuleGameControllers::Init()
 // Called every draw update
 update_status  ModuleGameControllers::PreUpdate()
 {
+	//Controller inputs
 
-	while (SDL_PollEvent(&event_)!=0) {
+	for (int i = 0; i < SDL_NumJoysticks(); ++i) {
 
-			//COndition if there is movement in the joystick axes
-			/*if (event_.type ==SDL_CONTROLLERAXISMOTION ) {
+		if (SDL_IsGameController(i)) {
+			if (i == 0) {
 
-				if(event_.jaxis.which==0){
+				Controller_player1 = SDL_GameControllerOpen(i);
+				//Checking if the controller is attached
+
+				if (SDL_GameControllerGetAttached(Controller_player1)) {
+					
+					//Here we assign the values of the aixis to the integers Controller_Player1_LAxisX...
+
+					Controller_player1_LAxisX = SDL_GameControllerGetAxis(Controller_player1, SDL_CONTROLLER_AXIS_LEFTX);
+					Controller_player1_LAxisY = SDL_GameControllerGetAxis(Controller_player1, SDL_CONTROLLER_AXIS_LEFTY);
+					
+					//Assign the boolean value to the  bools defined  
+
+					controller_player1_A_pressed = SDL_GameControllerGetButton(Controller_player1, SDL_CONTROLLER_BUTTON_A);
+				/*	controller_player1_RightShoulder_pressed = SDL_GameControllerGetButton(Controller_player1, SDL_CONTROLLER_BUTTON_RIGHTSHOULDER);*/
+					controller_player1_Start_pressed = SDL_GameControllerGetButton(Controller_player1, SDL_CONTROLLER_BUTTON_START);
+
+					Controller_player1_Connected = true;
+				}
 				
-					if (event_.jaxis.axis == 0) {
+				else {
+					
+					SDL_GameControllerClose(Controller_player1);
+					Controller_player1 = nullptr;
+					Controller_player1_Connected = false;
 
-						LOG("%d",event_.jaxis.value)
-
-						if (event_.jaxis.value > 8000) {
-							return update_status::UPDATE_STOP;
-
-						}
-						else if (event_.jaxis.value < -8000) {
-
-							return update_status::UPDATE_STOP;
-						}
-						
-					}
+				}
+			}
 			
-				}
-
-			}*/
-
-			//game controller buttons input
-
-			if (event_.type == SDL_CONTROLLERBUTTONDOWN) {
+			else if (i < 1) {
 				
-				LOG("%d", (int)event_.cbutton.button);
-
-				if (event_.cbutton.button == SDL_CONTROLLER_BUTTON_A) {
-						//do this
-					App->input->keyboard[SDL_SCANCODE_SPACE] = KEY_DOWN;
-				}
-				if (event_.cbutton.button == SDL_CONTROLLER_BUTTON_RIGHTSHOULDER) {
-
-					//do this
-
-				}
-
-
+				Controller_player2_Connected = false;
+				SDL_GameControllerClose(Controller_player2);
+				Controller_player2 = nullptr;
 
 			}
 
-		}
+			else if (i == 1 || i == 0 && Controller_player1_Connected == false) {
 
-		
+				Controller_player2 = SDL_GameControllerOpen(i);
+				if (SDL_GameControllerGetAttached(Controller_player2)) {
+
+					Controller_player2_LAxisX = SDL_GameControllerGetAxis(Controller_player2, SDL_CONTROLLER_AXIS_LEFTX);
+					Controller_player2_LAxisY = SDL_GameControllerGetAxis(Controller_player2, SDL_CONTROLLER_AXIS_LEFTY);
+
+					//Assign the boolean value to the  bools defined  
+
+					controller_player2_A_pressed = SDL_GameControllerGetButton(Controller_player2, SDL_CONTROLLER_BUTTON_A);
+				/*	controller_player2_RightShoulder_pressed = SDL_GameControllerGetButton(Controller_player2, SDL_CONTROLLER_BUTTON_RIGHTSHOULDER);*/
+					controller_player2_Start_pressed = SDL_GameControllerGetButton(Controller_player2, SDL_CONTROLLER_BUTTON_START);
+
+					Controller_player2_Connected = true;
+					break;
+
+
+				}
+				else {
+
+					SDL_GameControllerClose(Controller_player2);
+					Controller_player2 = nullptr;
+					Controller_player2_Connected = false;
+
+				}
+			}
+		}
+	}
+	
+	//Check Left Axis X & Y
+	if (Controller_player1_LAxisX > 6400) {
+
+		App->input->keyboard[SDL_SCANCODE_D] = KEY_STATE::KEY_REPEAT;
+
+	}
+	else if (Controller_player1_LAxisX<-DEATHZONE) {
+
+		App->input->keyboard[SDL_SCANCODE_A] = KEY_STATE::KEY_REPEAT;
+
+	}
+
+	if (Controller_player1_LAxisY < -DEATHZONE) {
+
+		App->input->keyboard[SDL_SCANCODE_W] = KEY_STATE::KEY_REPEAT;
+
+	}
+	else if (Controller_player1_LAxisY > DEATHZONE) {
+
+		App->input->keyboard[SDL_SCANCODE_S] = KEY_STATE::KEY_REPEAT;
+
+	}
+
+	//Check controller player one buttons
+
+	if (controller_player1_A_pressed == true && App->input->keyboard[SDL_SCANCODE_SPACE] == KEY_STATE::KEY_IDLE) {
+
+		App->input->keyboard[SDL_SCANCODE_SPACE] = KEY_STATE::KEY_DOWN;
+
+	}
+
+	else if (controller_player1_A_pressed==true) {
+	
+		App->input->keyboard[SDL_SCANCODE_SPACE] = KEY_STATE::KEY_REPEAT;
+	
+	}
+
+	//Check player two axes
+
+	if (Controller_player2_LAxisX > DEATHZONE) {
+
+		App->input->keyboard[SDL_SCANCODE_RIGHT] = KEY_STATE::KEY_REPEAT;
+
+	}
+	else if (Controller_player2_LAxisX<-DEATHZONE) {
+
+		App->input->keyboard[SDL_SCANCODE_LEFT] = KEY_STATE::KEY_REPEAT;
+
+	}
+
+	if (Controller_player2_LAxisY < -DEATHZONE) {
+
+		App->input->keyboard[SDL_SCANCODE_UP] = KEY_STATE::KEY_REPEAT;
+
+	}
+	else if (Controller_player2_LAxisY > DEATHZONE) {
+
+		App->input->keyboard[SDL_SCANCODE_DOWN] = KEY_STATE::KEY_REPEAT;
+
+	}
+
+	//Check controller player two buttons
+
+	if (controller_player2_A_pressed == true && App->input->keyboard[SDL_SCANCODE_RCTRL] == KEY_STATE::KEY_IDLE) {
+
+		App->input->keyboard[SDL_SCANCODE_RCTRL] = KEY_STATE::KEY_DOWN;
+
+	}
+
+	else if (controller_player2_A_pressed == true) {
+
+		App->input->keyboard[SDL_SCANCODE_RCTRL] = KEY_STATE::KEY_REPEAT;
+
+	}
 
 	
 	return update_status::UPDATE_CONTINUE;
@@ -111,17 +193,9 @@ update_status  ModuleGameControllers::PreUpdate()
 // Called before quitting
 bool  ModuleGameControllers::CleanUp()
 {
-	LOG("Cleanning moduleGameController")
-
-		for (int i = 0; i < MAX_GAME_CONTROLLERS; ++i)
-		{
-			if (PlayerController[i] != nullptr) {
-				SDL_GameControllerClose(PlayerController[i]);
-			}
-
-			PlayerController[i] = nullptr;
-
-		}
+	
+	LOG("Quitting SDL input event subsystem");
+	SDL_QuitSubSystem(SDL_INIT_EVENTS);
 
 	return true;
 }
